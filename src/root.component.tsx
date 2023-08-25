a4f13add0a0462269d98b08209b400377317ed7c
Dennis Kigen <kigen.work@gmail.com>
2023-08-25 11:11:27 +0300

    (chore) Tweak things to match new nomenclature

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -4,9 +4,9 @@ import Reporting from "./components/reporting/reporting.component";
 
 const Root: React.FC = () => {
   return (
-    <BrowserRouter basename={`${window.spaBase}/home/reporting`}>
+    <BrowserRouter basename={window.getOpenmrsSpaBase()}>
       <Routes>
-        <Route path="/" element={<Reporting />} />
+        <Route path="home/data-visualizer" element={<Reporting />} />
       </Routes>
     </BrowserRouter>
   );
