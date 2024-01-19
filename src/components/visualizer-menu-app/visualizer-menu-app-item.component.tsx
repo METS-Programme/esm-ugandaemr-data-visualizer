8a3801362c735f11489f9c6ed5fe7339ea557d12
Daphne210 <dapnephinerry2@gmail.com>
2024-01-19 17:15:38 +0300

    Adding the datA visualizer to the menu app

---
+++ src/components/visualizer-menu-app/visualizer-menu-app-item.component.tsx
@@ -0,0 +1,21 @@
+import { ClickableTile } from "@carbon/react";
+import React from "react";
+import styles from "./item.scss";
+import { Analytics } from "@carbon/react/icons";
+
+const Item = () => {
+  // items
+  const openmrsSpaBase = window["getOpenmrsSpaBase"]();
+
+  return (
+    <ClickableTile
+      className={styles.customTile}
+      id="menu-item"
+      href={`${openmrsSpaBase}stock-management`}
+    >
+      <div className="customTileTitle">{<Analytics size={24} />}</div>
+      <div>Data Visualizer</div>
+    </ClickableTile>
+  );
+};
+export default Item;
